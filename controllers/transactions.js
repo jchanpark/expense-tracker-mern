90884cb6c1f38db64f1a67ca63de791b68170924
jchanpark <junpark4936@gmail.com>
2022-10-13 10:50:07 -0400

    add routes for add and delete transactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -6,3 +6,19 @@ exports.getTransactions = (req, res, next) => {
   res.send('GET transactions');
 }
 
+// @desc  Add transaction
+// @route  POST /api/v1/transactions
+// @access  Public
+
+exports.addTransaction = (req, res, next) => {
+  res.send('POST transaction');
+}
+
+// @desc  Delete transactions
+// @route  DELETE /api/v1/transaction/:id
+// @access  Public
+
+exports.deleteTransaction = (req, res, next) => {
+  res.send('DELETE transaction');
+}
+
