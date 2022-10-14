aaa8b03f9526792353b964acf4396800993a7577
jchanpark <junpark4936@gmail.com>
2022-10-13 21:59:36 -0400

    use mongoose schema for transaction DB

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env'});
 
+connectDB();
+
 const transactions = require('./routes/transactions');
+const { connect } = require('mongoose');
 
 const app = express();
 
